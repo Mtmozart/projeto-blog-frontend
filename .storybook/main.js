78d3ed45edd046b27ea95acee213654bf0e1fd16
Mtmozart <mmsnborges@gmail.com>
2023-06-21 14:54:45 -0300

    initial

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,9 +1,12 @@
 module.exports = {
   //addons: ['storybook-version'],
-  stories: [
-    '../src/**/*.stories.mdx',
-    '../src/**/*.stories.@(js|jsx|ts|tsx)',
-    '../src/**/stories.@(js|jsx|ts|tsx)',
-  ],
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)', '../src/**/stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  framework: {
+    name: "@storybook/nextjs",
+    options: {}
+  },
+  docs: {
+    autodocs: true
+  }
 };
