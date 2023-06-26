8245b1383498e327b9eaec7a946d9e7b6671961a
Mtmozart <mmsnborges@gmail.com>
2023-06-26 12:40:35 -0300

    header

---
+++ src/components/Header/mock.ts
@@ -0,0 +1,28 @@
+import { HeaderProps } from '.';
+
+export default {
+  blogName: 'Fallen Calvo',
+  blogDescription:
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud.',
+  logo: 'https://pbs.twimg.com/tweet_video_thumb/EmabLdFWEAMLLcN.jpg',
+  showText: true,
+} as HeaderProps;
+
+/*export default {
+"data": {
+  "id": "1",
+  "attributes": {
+    "BlogName": "Alameda do dev",
+    "blogDescription": "O blog é dedicado a compartilhar conhecimentos e experiências de desenvolvedores em diversas áreas, oferecendo insights valiosos e dicas práticas para o aprimoramento das habilidades de programação. Aborda tópicos como linguagens de programação, frameworks, boas práticas de codificação, desenvolvimento web, mobile, inteligência artificial e muito mais. É uma fonte indispensável de informações atualizadas para desenvolvedores em busca de crescimento profissional e aprendizado contínuo.",
+    "logo": {
+      "data": {
+        "identificador": "3",
+        "attributes": {
+          "name": "3334910-desktop-source-code-and-wallpaper-by-coding-and-programming-gratis-foto.jpg",
+          "altText": null,
+          "url": "/assets/image/planet.jpg"
+        }
+      }
+     }
+    }
+    } as HeaderProps*/
